5a7fe979f4c1d0f766e6b59312467e05118461a1
Francis Lewis <fran@lovewales.net>
2022-10-27 22:10:09 +0100

    Create model class

---
+++ model/__init__.py
@@ -0,0 +1,25 @@
+class Model:
+    """
+    Class for image generation ML models.
+    Useful when devloping across platforms or versions (e.g. CPU only models vs GPU etc.) and to create model indepdent
+    pipelines
+    """
+
+    def __init__(self, _valid_kwargs=tuple(), **kwargs):
+
+        _valid_kwargs += ('model_name',  )
+        for k, v in kwargs.items():
+            if k not in _valid_kwargs:
+                raise ValueError("Keyword argument '{}' not supported".format(k))
+
+    def gen_txt2img(self, prompt):
+        """
+        prompt - string to be used as prompt for image generation
+        """
+        raise NotImplementedError
+
+    def gen_img2img(self, image):
+        """
+        image - python object pointing to an image
+        """
+        raise NotImplementedError
