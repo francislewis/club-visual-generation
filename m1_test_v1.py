c752bb8dc65d4699dbad87bb9242340f6eb03fe1
Jess Winterborne <98585585+JessWinterborne@users.noreply.github.com>
2022-11-17 22:19:36 +0000

    Fix imports

--- m1_test_v1.py
+++ m1_test_v1.py
@@ -1,7 +1,6 @@
-import torch
-from diffusers import StableDiffusionPipeline
-from Model import SD_GPU_V1_4
+from model.sd_gpu_v1_4 import SD_GPU_V1_4
 
+GPU_img2img_test_object = SD_GPU_V1_4(steps=5)
+image = GPU_img2img_test_object.gen_txt2img(prompt="Horse riding on an astronaut")
+display(image)
 
-GPU_img2img_test_object = SD_GPU_V1_4()
-print(GPU_img2img_test_object.gen_txt2img(prompt="Horse riding on an astronaut"))
